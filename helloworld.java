a45d927996c220b0628453f57132f1099721252f
Raymond <rlee00@stuy.edu>
2019-03-03 02:05:40 -0500

    hello world

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+/**********************************************
+ *  File Name:   HelloWorld.java
+ *  Compilation: javac HelloWorld.java
+ *  Execution:   java HelloWorld
+ *
+ *  Prints "hello, world". By tradition,
+ *  this is everyone's first program.
+**********************************************/
+
+public class {
+  public static void main(String[] commandLine) {
+    System.out.println("hello, world");
+  }
+}
